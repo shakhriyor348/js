66e8680749010392701923a36aa6438151bde716
shakhriyor <andrian.jr@mail.ru>
2020-06-01 23:26:06 +0500

    test

---
+++ script.js
@@ -0,0 +1,24 @@
+var name = prompt('Введите свое имя')
+console.log('Меня зовут ' + name );
+var year = +prompt('Введите свое имя')
+console.log('Мне ' + year );
+var primer1 = +prompt('решите пример 6 + 3 = ?')
+console.log('Пример 1: ' + ' 6 + 3 = ' + (6 + 3) + ' Ваш ответ ' + primer1);
+var primer2 = +prompt('решите пример 20 - 5 = ?')
+console.log('Пример 2: ' + ' 20 - 5 = ' + (20 - 5) + ' Ваш ответ ' + primer2);
+var primer3 = +prompt('решите пример 15 * 2 = ?')
+console.log('Пример 3: ' + ' 15 * 2 = ' + (15 * 2) + ' Ваш ответ ' + primer3);
+var primer4 = +prompt('решите пример 8 / 4 = ?')
+console.log('Пример 4: ' + ' 8 / 4 = ' + (8 / 4) + ' Ваш ответ ' + primer4);
+var primer5 = +prompt('решите пример 10 % 3 = ?')
+console.log('Пример 5: ' + ' 10 % 3 = ' + (10 % 3) + ' Ваш ответ ' + primer5);
+
+
+var str = ('Откройте консоль')
+console.log(str);
+alert(str)
+
+
+
+
+
